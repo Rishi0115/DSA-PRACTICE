cae87aa2255ba2eff96217b011175afa98066ed1
Rishi Jaiswal <jaiswalrishi.rj.15@gmail.com>
2026-06-24 16:26:55 +0530

    Implement Fibonacci function

---
+++ 17.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int fib(int n) {
+        if (n <= 1) {
+            return n;}
+        int prev2 = 0; 
+        int prev1 = 1; 
+        int current = 0;
+        for (int i = 2; i <= n; ++i) {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;}
+        return current;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    Solution sol;
+    cout << sol.fib(n) << endl;
+
+    return 0;
+}
